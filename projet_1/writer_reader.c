00400384d42a0286967bcfb88e5595d84911e363
camillecaulier <camillecaulier.cc@gmail.com>
2021-11-29 18:10:27 +0100

    writer reader

--- projet_1/writer_reader.c
+++ projet_1/writer_reader.c
@@ -8,53 +8,52 @@
 
 int n_w;
 int n_r;
-int current_w =0;
-int current_r =0;
+//int current_w =0;
+int r_current =0;
 pthread_mutex_t w_mutex;
 pthread_mutex_t r_mutex;
 sem_t r_sem;
 sem_t w_sem;
 void* writer(void* args){
     int w_count=0;
-    while(w_count<=640){
+    while(w_count<=20){
         w_count++;
 
-        pthread_mutex_lock(&w_mutex);
-        current_w++;
-        if(current_w == 1){
-            sem_wait(&r_sem); //if first writer block reader
-        }
-        pthread_mutex_unlock(&w_mutex);
-
         sem_wait(&w_sem);
-
-        //critiucal section
+        fprintf(stdout,"writing\n");
         while(rand()>RAND_MAX/10000);
-
         sem_post(&w_sem);
-        pthread_mutex_lock(&w_mutex);
-        w_count --;
-        if(w_count <= 0){ //wake up reader
-            sem_post(&r_sem);
-        }
-        pthread_mutex_unlock(&w_mutex);
 
     }
+    fprintf(stdout,"writing FINISHED\n");
     pthread_exit(NULL);
 }
 void* reader(void* args){
     int r_count=0;
-    while(r_count<=2560){
+    while(r_count<=30){
         r_count++;
         sem_wait(&r_sem);
         pthread_mutex_lock(&r_mutex);
-        r_count++;
-        if(r_count== 1){
+        r_current++;
+        if(r_current== 1){
             sem_wait(&w_sem); //if this is the first r then bloc w
         }
+        pthread_mutex_unlock(&r_mutex);
+        sem_post(&r_sem);
 
+        //execution critical
+        fprintf(stdout,"reading currently : %d\n",r_current);
+        while (rand() > RAND_MAX/10000);
 
+        pthread_mutex_lock(&r_mutex);
+        r_current--;
+        if(r_count<=0){
+            sem_post(&w_sem);//wake up writer when no writer
+        }
+        pthread_mutex_unlock(&r_mutex);
     }
+    fprintf(stdout,"READER FINSH\n");
+    pthread_exit(NULL);
 }
 
 int main(int argc, char** argv){
@@ -69,6 +68,9 @@ int main(int argc, char** argv){
 
     pthread_t w_threads[n_w];
     pthread_t r_threads[n_r];
+
+    sem_init(&w_sem,0,1);
+    sem_init(&r_sem,0,1);
 
     pthread_mutex_init(&w_mutex,NULL);
     pthread_mutex_init(&r_mutex, NULL);
@@ -95,5 +97,8 @@ int main(int argc, char** argv){
     }
     pthread_mutex_destroy(&w_mutex);
     pthread_mutex_destroy(&r_mutex);
+    sem_destroy(&w_sem);
+    sem_destroy(&r_sem);
+
 
 }
