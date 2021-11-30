402a73572fa8939fa83dbc76f2f97b5f88c3588d
camillecaulier <camillecaulier.cc@gmail.com>
2021-11-30 16:32:49 +0100

    shell

--- projet_1/philosopher_shell.sh
+++ projet_1/philosopher_shell.sh
@@ -2,15 +2,15 @@
 NTHREADS=16
 NSAMPLE=5
 #make philosopher_problem
-rm -f philosopher_problem.CSV
-echo "n_thread","time" >> philosopher_problem.CSV
+rm -f data/philosopher_problem.CSV
+echo "n_thread","time" >> data/philosopher_problem.CSV
 for((i=1;i<=NTHREADS;i++))
 do
   echo $i "threads"
   for((j=1;j<=NSAMPLE;j++))
   do
     OUTPUT=$((/usr/bin/time -f %e ./philosopher_problem $i 2>&1) | cut -d\) -f2)
-    echo $i, $OUTPUT >> philosopher_problem.CSV
+    echo $i, $OUTPUT >> data/philosopher_problem.CSV
   done
 done
 rm -f philosopher_problem
--- projet_1/writer_reader_shell.sh
+++ projet_1/writer_reader_shell.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 NTHREADS=16
 NSAMPLE=5
-make writer_reader
-rm -f writer_reader.CSV
-echo "n_thread","time" >> writer_reader.CSV
+
+rm -f data/writer_reader.CSV
+echo "n_thread","time" >> data/writer_reader.CSV
 for((i=1;i<=NTHREADS;i++))
 do
   echo $i "threads"
   for((j=1;j<=NSAMPLE;j++))
   do
     OUTPUT=$((/usr/bin/time -f %e ./writer_reader $i $i 2>&1) | cut -d\) -f2)
-    echo $i, $OUTPUT >> writer_reader.CSV
+    echo $i, $OUTPUT >> data/writer_reader.CSV
+  done
+done
+rm -f data/writer_reader_2.CSV
+echo "n_thread","time" >> data/writer_reader_2.CSV
+for((i=1;i<=NTHREADS;i++))
+do
+  echo $i "threads"
+  for((j=1;j<=NSAMPLE;j++))
+  do
+    OUTPUT=$((/usr/bin/time -f %e ./writer_reader $i $i 2>&1) | cut -d\) -f2)
+    echo $i, $OUTPUT >> data/writer_reader.CSV
   done
 done
 rm -f writer_reader
