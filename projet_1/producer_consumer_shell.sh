fa1a50d5f8f247d1067af305f421a19502de4c70
plazex <noah.gingu@gmail.com>
2021-11-30 17:09:46 +0100

    modif in writer reader

--- projet_1/producer_consumer_shell.sh
+++ projet_1/producer_consumer_shell.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 NTHREADS=16
 NSAMPLE=5
-make producer_consumer
+#make producer_consumer
 rm -f producer_consumer.CSV
-echo "n_thread","time" >> producer_consumer.CSV
+echo "n_thread","time" >> data/producer_consumer.CSV
+for((i=1;i<=NTHREADS;i++))
+do
+  echo $i "threads"
+  for((j=1;j<=NSAMPLE;j++))
+  do
+    OUTPUT=$((/usr/bin/time -f %e ./producer_consumer $i $i 2>&1) | cut -d\) -f2)
+    echo $i, $OUTPUT >> data/producer_consumer.CSV
+  done
+done
+echo "n_thread","time" >> data/producer_consumer_2.CSV
 for((i=1;i<=NTHREADS;i++))
 do
   echo $i "threads"
--- projet_1/writer_reader_shell.sh
+++ projet_1/writer_reader_shell.sh
@@ -13,6 +13,8 @@ do
     echo $i, $OUTPUT >> data/writer_reader.CSV
   done
 done
+
+
 rm -f data/writer_reader_2.CSV
 echo "n_thread","time" >> data/writer_reader_2.CSV
 for((i=1;i<=NTHREADS;i++))
@@ -20,8 +22,44 @@ do
   echo $i "threads"
   for((j=1;j<=NSAMPLE;j++))
   do
-    OUTPUT=$((/usr/bin/time -f %e ./writer_reader $i $i 2>&1) | cut -d\) -f2)
-    echo $i, $OUTPUT >> data/writer_reader.CSV
+    OUTPUT=$((/usr/bin/time -f %e ./writer_reader $4 $i 2>&1) | cut -d\) -f2)
+    echo $i, $OUTPUT >> data/writer_reader_2.CSV
+  done
+done
+
+
+echo "n_thread","time" >> data/producer_consumer_3.CSV
+for((i=1;i<=NTHREADS;i++))
+do
+  echo $i "threads"
+  for((j=1;j<=NSAMPLE;j++))
+  do
+    OUTPUT=$((/usr/bin/time -f %e ./producer_consumer $12 $i 2>&1) | cut -d\) -f2)
+    echo $i, $OUTPUT >> data/producer_consumer_3.CSV
+  done
+done
+
+
+echo "n_thread","time" >> data/producer_consumer_4.CSV
+for((i=1;i<=NTHREADS;i++))
+do
+  echo $i "threads"
+  for((j=1;j<=NSAMPLE;j++))
+  do
+    OUTPUT=$((/usr/bin/time -f %e ./producer_consumer $i $4 2>&1) | cut -d\) -f2)
+    echo $i, $OUTPUT >> data/producer_consumer_4.CSV
+  done
+done
+
+
+echo "n_thread","time" >> data/producer_consumer_5.CSV
+for((i=1;i<=NTHREADS;i++))
+do
+  echo $i "threads"
+  for((j=1;j<=NSAMPLE;j++))
+  do
+    OUTPUT=$((/usr/bin/time -f %e ./producer_consumer $i $12 2>&1) | cut -d\) -f2)
+    echo $i, $OUTPUT >> data/producer_consumer_5.CSV
   done
 done
 rm -f writer_reader
