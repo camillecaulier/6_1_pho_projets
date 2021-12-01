475c37a5765d28a6c50ad0e011f337dc6cc25bc3
plazex <noah.gingu@gmail.com>
2021-12-01 16:27:33 +0100

    3rd try .py

--- projet_1/philosopher_problem.py
+++ projet_1/philosopher_problem.py
@@ -7,15 +7,21 @@ ph_pb_data = pd.read_csv("philosopher_problem.CSV")
 #Mean and standard deviation of the measures
 
 mean = ph_pb_data.groupby(["n_threads"]).mean()["time"]
-std = ph_pb_data.groupby(["n_threads"]).std()["time"]
+std_dev = ph_pb_data.groupby(["n_threads"]).std()["time"]
 
-n_threads = ph_pb_data["n_threads"]
+n_threads = ph_pb_data["n_threads"].unique()
 
 #Labels and Title
 plt.xlabel("Number of Threads")
 plt.ylabel("Execution time")
 plt.title("Execution time according to the number of threads")
+plt.grid(True)
 
+#Creating the figure
 fig = plt.figure()
-
-plt.plot()
+plt.plot(n_threads, mean, linewidth=1.2)
+plt.xlim(0, 20)
+plt.ylim(0,3)
+plt.savefig("philosopher_problem.PNG")
+plt.show()
+plt.close()
