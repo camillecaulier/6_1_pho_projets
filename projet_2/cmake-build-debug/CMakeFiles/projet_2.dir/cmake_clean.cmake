9877e2a0d024b2f09c118b326d86a09c3ddf6ac7
camillecaulier <camillecaulier.cc@gmail.com>
2021-12-14 23:12:24 +0100

    we begin

---
+++ projet_2/cmake-build-debug/CMakeFiles/projet_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projet_2.dir/main.c.o"
+  "projet_2"
+  "projet_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/projet_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
